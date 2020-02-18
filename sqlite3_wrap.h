66d6e980813403cde27f5802f86f8ef12c4186ba
PK2n <pratik_khanke005@yahoo.com>
2020-02-18 21:17:08 +0530

    Added support for lambda expression and std::function to be used as callback functions. Now error code is stored and can be retrieved.

--- sqlite3_wrap.h
+++ sqlite3_wrap.h
@@ -1,20 +1,40 @@
-#ifndef SQLITEDB_H
-#define SQLITEDB_H
+#ifndef SQLITE3_WRAP_H
+#define SQLITE3_WRAP_H
 
 #include <string>
+#include <functional>
 
 #include <sqlite3.h>
+
+class FunObj
+{
+    std::function<int(int,char**,char**)> func;
+public:
+    FunObj(std::function<int(int,char**,char**)> fun):func{fun} {}
+    inline int operator()(int numOfColumns, char **columnData, char **columnName)
+    {
+        return func(numOfColumns, columnData, columnName);
+    }
+};
+
+inline int dbCallback(void* arg, int numOfColumns, char **columnData, char **columnName)
+{
+    auto func = static_cast<FunObj*>(arg);
+    
+    return (*func)(numOfColumns,columnData,columnName);
+}
 
 class SQLite3DB
 {
     sqlite3 *db;
     std::string name;
     std::string error;
+    int ec = 0;
 
 public:
     SQLite3DB(std::string n) : name{n}
     {
-        int ec = sqlite3_open(name.c_str(),&db);
+        ec = sqlite3_open(name.c_str(),&db);
         if(ec)
             error = "unable to open database";
     }
@@ -26,15 +46,19 @@ public:
 
     sqlite3* get() { return db; }
     std::string getError() const { return error; }
+    int getErrorCode() const { return ec; }
 
-    bool queryExec(std::string query, int (*callback) (void* arg,int numOfColumns, char **columnData, char **columnName) = nullptr,void* callbackArg=nullptr);
+    inline bool queryExec(std::string query, int (*callback) (void* arg,int numOfColumns, char **columnData, char **columnName) = nullptr,void* callbackArg=nullptr);
+    
+    inline bool queryExec(std::string query,std::function<int(int,char**,char**)> func);
 };
 
 bool SQLite3DB::queryExec(std::string query, int (*callback) (void* arg,int numOfColumns, char **columnData, char **columnName),void* callbackArg)
 {
     char* err = nullptr;
-    int ec = sqlite3_exec(db,query.c_str(),callback,callbackArg,&err);
-
+    
+    ec = sqlite3_exec(db,query.c_str(),callback,callbackArg,&err);
+    
     if(ec != SQLITE_OK)
     {
         if(err)
@@ -47,4 +71,11 @@ bool SQLite3DB::queryExec(std::string query, int (*callback) (void* arg,int numO
     return true;
 }
 
-#endif // SQLITEDB_H
+bool SQLite3DB::queryExec(std::string query,std::function<int(int,char**,char**)> func)
+{
+    FunObj fun{func};
+    
+    return queryExec(query,dbCallback,&fun);
+}
+
+#endif // SQLITE3_WRAP_H
